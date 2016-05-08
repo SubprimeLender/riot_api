10bb160e69685df0e38908a4f5772d2e85ac8fe7
Tony <li.tony98@gmail.com>
2016-05-08 02:00:23 -0600

    Update function.php

--- function.php
+++ function.php
@@ -2,6 +2,7 @@
 include 'api_key.php';
 include 'region.php';
 include 'get_api_function.php';
+include 'get_mastery_function.php';
 
 //get summoner name from form
 $summoner_name_encoded = rawurlencode($_POST['summonername']);
@@ -20,23 +21,7 @@ $summoner_name = strtolower($summoner_name);
 		  "Name"        => $data->summonerName, 
 		  "ID"          => $data->summonerId, 
 		  "Champion"    => $data->championId,
-		  "Team"		=> $data->teamId,
+		 "Team"		=> $data->teamId,
+		  "CMastery"	=> get_mastery_function($data->summonerId),
 		  );
-	};
-
-//gets champion mastery data for all players
-	//array of $match_players' keys
-	$players = array_keys($match_players);
-	//iterates through each of the 10 players
-	foreach ($players as $player) {
-		//get champion mastery for particular $player
-		$parsed_champion_mastery = get_api("https://na.api.pvp.net/championmastery/location/NA1/player/" . $player . "/champions?api_key=" . $api_key_tony);
-		//create array of particular player's champion masteries 
-		$mastery_array = array();
-		//store champion masteries into $mastery_array {championId => championPoints} 
-		foreach ($parsed_champion_mastery as $champ) {
-			$mastery_array[$champ->championId] = $champ->championPoints;
-		};
-		//add $mastery_array to storage $match_players as additional array [0]
-		array_push($match_players[$player], $mastery_array);
 	};
