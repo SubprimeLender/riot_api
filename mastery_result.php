ea05abf0120c0684788c4d0a0c638dfd46b0a58a
Tony <li.tony98@gmail.com>
2016-05-08 13:55:27 -0600

    Update mastery_result.php

--- mastery_result.php
+++ mastery_result.php
@@ -1,4 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Champselect.info</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
 <?php
 include 'function.php';
+?>
+<div class="container">
+	<div class="row">
+		<div id="top-team" class="col-lg-12 col-md-12 hidden-sm hidden-xs">
+			sup
+		</div>
+		<div id="bottom-team" class="col-lg-12 col-md-12 hidden-sm hidden-xs">
+			hi
+		</div>
+	</div>
+</div>
 
-print_r($match_players);
+</body>
+</html>
