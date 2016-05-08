4b3638a3cc375c69580027f365204b64b02268d8
Tony <li.tony98@gmail.com>
2016-05-08 02:06:39 -0600

    Update get_mastery_function.php

--- get_mastery_function.php
+++ get_mastery_function.php
@@ -1,5 +1,6 @@
 <?php
 function get_mastery_function($summonerId) {
+	//get champion masteries from API
 	$parsed_champion_mastery = get_api("https://na.api.pvp.net/championmastery/location/NA1/player/" . $summonerId . "/champions?api_key=5e54590d-31c4-4339-ab8f-dc872fd9dffe");
 	//create array of particular player's champion masteries 
 	$mastery_array = array();
