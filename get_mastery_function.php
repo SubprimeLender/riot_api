7e4d19fd0f7e9cd0a72fbd3196341bba8deae04e
Tony <li.tony98@gmail.com>
2016-05-08 10:07:53 -0600

    Update get_mastery_function.php

--- get_mastery_function.php
+++ get_mastery_function.php
@@ -1,10 +1,13 @@
 <?php
-function get_mastery_function($summonerId) {
+/*Function get_mastery()
+*@param summoner ID
+*@return array of all summoner's {championId => championPoints}
+*/
+function get_mastery($summonerId) {
 	//get champion masteries from API
 	$parsed_champion_mastery = get_api("https://na.api.pvp.net/championmastery/location/NA1/player/" . $summonerId . "/champions?api_key=5e54590d-31c4-4339-ab8f-dc872fd9dffe");
-	//create array of particular player's champion masteries 
-	$mastery_array = array();
 	//store champion masteries into $mastery_array {championId => championPoints} 
+	$mastery_array = array();
 	foreach ($parsed_champion_mastery as $champ) {
 		$mastery_array[$champ->championId] = $champ->championPoints;
 	};
