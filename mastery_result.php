75e7bdc858e957db6c37e51a6eec873b22020536
Tony <li.tony98@gmail.com>
2016-05-08 17:52:04 -0600

    Update mastery_result.php

--- mastery_result.php
+++ mastery_result.php
@@ -10,15 +10,59 @@
 
 <body>
 <?php
-include 'function.php';
+	include 'function.php';
+	include 'ddragon_version_url.php';
+	include 'champion_reference.php';
+	//split $match_players into first 5 top team and last 5 bottom team
+	$match_teams = array_chunk($match_players, 5);
 ?>
+<!-- Top of page heading -->
+<div class ="jumbotron jumbotron-fluid">
+	<div class ="container text-center">
+		<h1>Current Game</h1>
+	</div>
+</div>
 <div class="container">
-	<div class="row">
+	<div class="row text-center">
 		<div id="top-team" class="col-lg-12 col-md-12 hidden-sm hidden-xs">
-			sup
+			<table class="table">
+			<tbody>
+				<tr>
+					<?php
+					foreach($match_teams[0] as $k) {
+						echo "<td><img src='http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" . $champion_reference[$k["Champion"]] . "_0.jpg' height='270' width='150'></td>";	
+					};
+					?>
+				</tr>
+				<tr>
+				<?php
+					foreach($match_teams[0] as $k) {
+						echo "<td>" . $k['Name'] . "</td>";	
+					};
+					?>
+				</tr>
+			</tbody>
+			</table>
 		</div>
 		<div id="bottom-team" class="col-lg-12 col-md-12 hidden-sm hidden-xs">
-			hi
+			<table class="table">
+			<tbody>
+				<tr>
+					<?php
+					foreach($match_teams[1] as $k) {
+						echo "<td>" . $k['Name'] . "</td>";	
+					};
+					?>
+				</tr>
+				<tr>
+					<?php
+					foreach($match_teams[1] as $k) {
+						echo "<td><img src='http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" . $champion_reference[$k["Champion"]] . "_0.jpg' height='270' width='150'></td>";	
+					};
+					?>
+				</tr>
+			</tbody>
+			</table>
 		</div>
 	</div>
 </div>
