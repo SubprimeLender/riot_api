7fc0a6b829e8ae4395926fb82217de1c904ca020
Tony <li.tony98@gmail.com>
2016-05-09 17:54:43 -0500

    Update name_submit.php

--- name_submit.php
+++ name_submit.php
@@ -1,12 +1,28 @@
 <html>
 <head>
-<title>submit name</title>
+<title>Champselect.info</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="custom_styles.css">
 </head>
+<style>
+	.top-pad {
+		padding-top: 500px;
+	}
+</style>
 <body>
-<form action="mastery_result.php" method="POST">
-	Summoner Name:
-	<input type="text" name="summonername" id="summonername">
-	<input type="submit" value="Submit">
-</form>
+<!-- Top of page heading -->
+<div id="logo" class="center top-pad col-12">
+	
+</div>
+<div id="submit" class="center text-center col-12">
+	<form action="mastery_result.php" method="POST">
+		<input type="text" name="summonername" id="summonername">
+		<div>
+	    	<input type="submit" value="Look Me Up!" class="btn btn-success btn-lrg">
+	    </div>
+	</form>
+</div>
 </body>
 </html>
